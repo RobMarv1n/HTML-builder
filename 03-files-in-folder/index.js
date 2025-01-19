83a8f4123730b5b7732fce8ac3b2c9d8f11a63f1
Rob Marvin <fenalfan@gmail.com>
2025-01-19 03:55:47 +0300

    feat: complete 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs/promises');
+const path = require('path');
+const { stdout } = require('process');
+const folderPath = path.join(__dirname, 'secret-folder\\');
+
+async function showFiles(directoryPath) {
+  const filesWithTypes = await fs.readdir(directoryPath, {
+    withFileTypes: true,
+  });
+  for (let fileWithType of filesWithTypes) {
+    const fileStats = await fs.stat(`${directoryPath + fileWithType.name}`);
+    if (fileWithType.isFile()) {
+      stdout.write(fileWithType.name.split('.')[0] + ' - ');
+      stdout.write(path.extname(fileWithType.name + ' - '));
+      stdout.write((fileStats.size / 1024).toFixed(3) + 'kb\n');
+    }
+  }
+}
+
+showFiles(folderPath);
